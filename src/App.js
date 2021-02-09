1062b3fd307d86d118df0427ebed54a5d0c353c0
Vladyslav Koval <49593840+Vldkvll@users.noreply.github.com>
2021-02-09 02:26:41 +0200

    installed depend.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+export function App() {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='chatdev'
+			userSecret='123123'
+			projectID='7999f6db-b355-4669-88b7-2565e03d0b2c'
+		/>
+	);
 }
 
-export default App;
+            export default App;
+            
